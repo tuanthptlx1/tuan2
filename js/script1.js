e49291b29b3aa32f57f01e14440c50bc1752ddd9
tuanthptlx1 <nt7304398@gmail.com>
2024-05-28 21:33:20 +0700

    .
    .

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-function toggleGender(element) {
-    var genderDiv = element.nextElementSibling; // Lấy phần tử liền kề
-    if (genderDiv.style.display === "none") {
-        genderDiv.style.display = "block";
-    } else {
-        genderDiv.style.display = "none";
-    }
-}
-
